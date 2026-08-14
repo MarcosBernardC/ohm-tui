0c617c524172a12c7af025d61db15c739a546cef
MarcosBernardC <marcosbernardcalixto@gmail.com>
2026-08-14 11:24:00 -0500

    chore: inicializar estructura base del proyecto

---
+++ project.py
@@ -0,0 +1,16 @@
+def req_func_1():
+    pass
+
+def req_func_2():
+    pass
+
+def req_func_3():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_project.py
@@ -0,0 +1,10 @@
+import pytest
+
+def test_req_func_1():
+    pass
+
+def test_req_func_2():
+    pass
+
+def test_req_func_3():
+    pass
