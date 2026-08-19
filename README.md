d7bf974ebca0b6a20c906e5b2895c13a34a7f348
MarcosBernardC <marcosbernardcalixto@gmail.com>
2026-08-19 12:25:56 -0500

    feat(ui): implementar MenuView0 y actualizar diagrama de secuencia del cursor

--- README.md
+++ README.md
@@ -27,8 +27,36 @@ classDiagram
 
 ```mermaid
 sequenceDiagram
-   Bernard->>+tui:  python project.py
-    tui-->>-Bernard: Menu General (opt:0)
-    Bernard->>+tui: Presionar 'j'
-    tui-->>-Bernard:  Menu General (opt:1)
+   Bernard->>+kitty: python project.py
+   kitty->>+MenuView0: getview()
+   MenuView0->>+Cursor: set_CursorLimits(Ymax, Xmax)
+   MenuView0->>+Cursor: getCursorPos()
+   Cursor-->>-MenuView0: SendCursorPos"(Y=0, X=0)"
+   MenuView0-->>-Bernard: MenuList[0]
+   
+   rect rgb(240, 248, 255)
+   opt Si presiona 'j'
+      Bernard->>+InputHandler: presionar 'j'
+      InputHandler-->>Bernard: print("presionaste j!")
+      alt Si CursorY < LímiteYmax
+         InputHandler->>Cursor: setCursorYPos("+1")
+      else Límite superado
+         InputHandler-->>Bernard: print("límite Ymax superado")
+      end
+      deactivate InputHandler
+   end
+   end
+
+   rect rgb(255, 240, 245)
+   opt Si presiona 'k'
+      Bernard->>+InputHandler: presionar 'k'
+      InputHandler-->>Bernard: print("presionaste k!")
+      alt Si CursorY > LímiteYmin
+         InputHandler->>Cursor: setCursorYPos("-1")
+      else LímiteY mínimo superado
+         InputHandler-->>Bernard: print("límite Ymin superado")
+      end
+      deactivate InputHandler
+   end
+   end
 ```
--- project.py
+++ project.py
@@ -1,11 +1,13 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from modules.terminal import Terminal
+
 
 @dataclass
 class Cursor:
+    symbol: str = '>'
     linea: int = 0
     columna: int = 0
-    max_posicion: tuple[int, int] = (10, 10)
+    max_posicion: tuple[int, int] = (10, 0)
 
     def mover_arriba(self):
         if self.linea > 0:
@@ -24,27 +26,42 @@ class Cursor:
             self.columna -= 1
 
     @property
-    def posicion(self) -> tuple[int, int]:
+    def posicion(self):
         return (self.linea, self.columna)
 
     @posicion.setter
-    def posicion(self, posicion: tuple[int, int]):
-        if ((0 <= posicion[0] <= self.max_posicion[0]) and 0 <= posicion[1] <= self.max_posicion[1]):
-            linea, columna = posicion
-            self.linea = linea
-            self.columna = columna
+    def posicion(self, value):
+        pass
+
+@dataclass
+class MenuView0:
+    cursor: Cursor=field(default_factory=lambda:Cursor(max_posicion=(1,0)))
+    opt: list=field(default_factory=lambda:["1. opt a", "2. opt b"])
+
+    def render(self): 
+        menu_str = []
+        for i, str_opt in enumerate(self.opt):
+            if i == self.cursor.linea:
+                menu_str.append(f"{self.cursor.symbol} {str_opt}")
+            else:
+                menu_str.append(str_opt)
+
+        print('\n'.join(menu_str))
 
 def main():
     # Terminal.mover_cursor(7,7)
     # print("Línea 7, Columna 7!")
 
-    cursor = Cursor()
-    print(cursor)
-    print(cursor.posicion)
+    # cursor = Cursor()
+    # print(cursor)
+    # print(cursor.posicion)
+    #
+    # cursor.mover_abajo()
+    # print(cursor)
+    # print(cursor.posicion)
 
-    cursor.mover_abajo()
-    print(cursor)
-    print(cursor.posicion)
+    menu = MenuView0()
+    menu.render()
 
 if __name__ == "__main__":
     main()
