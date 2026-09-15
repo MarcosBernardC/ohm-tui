6e7cdcee17c2adb63cf2dffa5442158e5fe04049
bernard <bernardlab.dev@gmail.com>
2026-09-15 17:40:48 -0500

    feat: ampliar suite de tests para el proyecto.
    - Navegar hasta cada submenu para pruebas de corriente, voltaje y
    rwsistencia.

--- test_project.py
+++ test_project.py
@@ -76,9 +76,22 @@ data_normalizar_parametro = [
         (['l'], "resistencia", 453200134, "453.20 MΩ"),
 
         #CALCULO CORRIENTE
-        (['j', 'l'], "voltaje", 0.0000043, "4.30 uV"),
+        (['j', 'l'], "corriente", 0.000000573, "573.00 nA"),
+        (['j', 'l'], "corriente", 4.367, "4.37 A"),
+        (['j', 'l'], "corriente", 48767004, "48.77 MA"),
 
-        ] # NORMALIZAR VOLTAJE
+        (['j', 'l'], "resistencia", 0.000000573, "573.00 nΩ"),
+        (['j', 'l'], "resistencia", 220.578, "220.58 Ω"),
+        (['j', 'l'], "resistencia", 12345000, "12.35 MΩ"),
+        (['j', 'l'], "voltaje", 0.000015798, "15.80 uV"),
+        (['j', 'l'], "voltaje", 12.688, "12.69 V"),
+        (['j', 'l'], "voltaje", 15234760000, "15.23 GV"),
+        #CALCULO RESISTENCIA
+        (['j', 'j', 'l'], "resistencia", 0.000000573, "573.00 nΩ"),
+        (['j', 'j', 'l'], "resistencia", 1.0458, "1.05 Ω"),
+        (['j', 'j', 'l'], "resistencia", 20067835, "20.07 MΩ"),
+
+        ] # NORMALIZAR
 @pytest.mark.parametrize("secuencia_entrada, nombre_parametro, valor, valor_normalizado", data_normalizar_parametro)
 def test_normalizar_parametro(secuencia_entrada, nombre_parametro, valor, valor_normalizado):
     assert normalizar_parametro(secuencia_entrada, nombre_parametro, valor) == valor_normalizado
